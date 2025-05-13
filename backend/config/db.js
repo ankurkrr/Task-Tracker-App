59539e1abb38d4ca3d6309aa5cb718345b8d011b
Ankur Kumar <11.ankurkr@gmail.com>
2025-05-14 00:17:09 +0530

    change db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,9 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("MongoDB connected...");
+    await mongoose.connect(process.env.MONGO_URI); // Remove useNewUrlParser and useUnifiedTopology
+    console.log('MongoDB connected');
   } catch (err) {
     console.error(err.message);
     process.exit(1);
